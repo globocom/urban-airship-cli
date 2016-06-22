e57804fc1c6df6f99416ffaae1484c0c10a5b715
Arthur Zettler <arthur.zettler@corp.globo.com>
2016-06-22 15:14:47 -0300

    chore: add support for dynamic app secret and app key usage; resolve #1

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
 var program = require('commander');
 var request = require('request');
 
-program
-	.command('notify [message]')
-	.description('send notification to all application devices')
-	.action(function (message) {
-		console.log('Notifing:', message);
+function encodeCredential (credential) {
+	return new Buffer(credential).toString('base64');
+}
 
-		request({
-			method: 'POST',
-			url: 'https://go.urbanairship.com/api/push/',
-			
-			json: {
-				'audience': 'all',
-				'notification': {'alert': message},
-				'device_types': ['android'],
-			},
+function loadCredential (program) {
+	var secret = null;
+	var key = null;
+	var credential = null;
 
-			headers: {
-				'Authorization': 'Basic ' + new Buffer('w9rweopTR869zt5jjHzgiA:8816ZB4GQ7aJokhT9CUIcA').toString('base64'),
-				'Accept': 'application/vnd.urbanairship+json; version=3;',
-				'Content-Type': 'application/json',
-			},
-		},
+	if (program.secret) secret = program.secret;
+	else if (process.env.URBAN_AIRSHIP_MASTER_SECRET) secret = process.env.URBAN_AIRSHIP_MASTER_SECRET;
+	else throw new Error('application master key not defined');
+
+	if (program.key) key = program.key;
+	else if (process.env.URBAN_AIRSHIP_KEY) key = process.env.URBAN_AIRSHIP_KEY;
+	else throw new Error('application key not defined');
+
+	credential = key + ':' + secret;
+
+	return credential;
+}
+
+function createPushData (message, credential) {
+	var requestPushData = {
+		method: 'POST',
+		url: 'https://go.urbanairship.com/api/push/',
 		
-		function (e, r, body) {
-			if (e) return console.log('Error: ', e);
+		json: {
+			'audience': 'all',
+			'notification': {'alert': message},
+			'device_types': ['android'],
+		},
 
-			console.log('Notification sent:', body);
-		});
-	});
+		headers: {
+			'Authorization': 'Basic ' + encodeCredential(credential),
+			'Accept': 'application/vnd.urbanairship+json; version=3;',
+			'Content-Type': 'application/json',
+		},
+	}
+
+	return requestPushData;
+}
+
+function requestPushHandler (error, request, body) {
+	if (error) return console.log('Error: ', error);
+
+	console.log('Notification sent:', body);
+}
+
+function pushAction (message) {
+	var credential = null;
+	var requestPushData = null;
+
+	console.log('Notifing you app with message:', message);
+
+	try {
+		credential = loadCredential(program);
+		requestPushData = createPushData(message, credential);
+
+		request(requestPushData, requestPushHandler);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+program
+	.option('-s, --secret <n>', 'application master secret from urban airship')
+	.option('-k, --key <n>', 'application key from urban airship')
+	.command('push <message>')
+	.description('send push notification to all application devices')
+	.action(pushAction);
 
 program.parse(process.argv);
