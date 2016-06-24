ab077e0cc91ab8498d678e273c3a0e583c85d3b8
Arthur Zettler <arthur.zettler@corp.globo.com>
2016-06-24 13:47:02 -0300

    chore: add command line version from package.json; resolve #6

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var arguments = process.argv;
 
 var broadcastCommand = require('./commands/broadcast');
 var actionUrlCommand = require('./commands/action-url');
-
+var package = require('./package');
 
 var commands = [broadcastCommand,
 				actionUrlCommand];
@@ -18,6 +18,7 @@ commands.forEach(function (command) {
 });
 
 program
+	.version(package.version)
 	.option('-s, --secret <n>', 'application master secret from urban airship')
 	.option('-k, --key <n>', 'application key from urban airship');
 
